302b7b3038307be6099f27069febe10c3dbc8cb6
RyseJuan <juangomezdiego@gmail.com>
2026-03-17 15:12:22 -0500

    subir archivo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App(){
+
+  return(
+    <div className="min-h-screen bg-green-800 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        Chavez vive la patria sigue
+      </h1>
+    </div>
+
+
+  );
+  
+}
